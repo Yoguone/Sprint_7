916993bcfdbcc39acad114a25538137204dddc83
Alex Andr <yogua@yandex.ru>
2025-04-08 18:18:47 +0300

    fixture and authorization test refactoring

--- conftest.py
+++ conftest.py
@@ -1,14 +1,23 @@
 import requests
 from urls import Urls
 import pytest
+from helpers import register_new_courier_and_return_login_password
 
 @pytest.fixture
 def delete_courier():
-    def _delete_courier(courier_id):
-        delete_courier_url = Urls.delete_courier_url
-        response = requests.delete(f'{delete_courier_url}/{courier_id}')
-        return response
-    yield _delete_courier
+    courier_data = register_new_courier_and_return_login_password()
+    yield courier_data
+    login, password, first_name = courier_data
+    authorization_data = {"login": login,
+                          "password": password}
+    response = requests.post(Urls.courier_authorization, authorization_data)
+    response_json = response.json()
+    courier_id = response_json['id']
+    delete_courier_url = Urls.delete_courier_url
+    requests.delete(f'{delete_courier_url}/{courier_id}')
+
+
+
 
 
 
--- tests/test_courier_authorization.py
+++ tests/test_courier_authorization.py
@@ -7,19 +7,13 @@ from data import ResponseMessages
 class TestCourierAuthorization:
 
     @allure.title("Успешная авторизация курьера")
-    def test_courier_authorization_successful(self, delete_courier):
-        courier_data = register_new_courier_and_return_login_password()
-        login, password, first_name = courier_data
-        courier_authorization_url = Urls.courier_authorization
+    def test_courier_authorization_successful(self,delete_courier):
+        login, password, first_name = delete_courier
         authorization_data = {"login": login,
-                   "password": password}
-        response = requests.post(courier_authorization_url, authorization_data)
+                              "password": password}
+        response = requests.post(Urls.courier_authorization, authorization_data)
         response_json = response.json()
         assert response.status_code == 200 and 'id' in response_json
-        courier_id = response_json['id']
-        delete_response = delete_courier(courier_id)
-        delete_response_json = delete_response.json()
-        assert delete_response.status_code == 200 and delete_response_json == {"ok":True}
 
     @allure.title("Авторизация курьера без пароля")
     def test_courier_authorization_without_password_failed(self):
